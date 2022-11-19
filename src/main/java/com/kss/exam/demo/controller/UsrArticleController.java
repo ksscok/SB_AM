8cd4bbab28b7d7776f00b2f3717d10c29ef7b561
ksscok <ksscok1@gmail.com>
2022-11-19 16:50:14 +0900

    ArticleController 게시물 등록, 게시물 리스트 구현

---
+++ src/main/java/com/kss/exam/demo/controller/UsrArticleController.java
@@ -0,0 +1,43 @@
+package com.kss.exam.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kss.exam.demo.vo.Article;
+
+@Controller
+public class UsrArticleController {
+	
+	private int lastArticleId;
+	List<Article> articles;
+	
+	UsrArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		
+		int id = lastArticleId + 1;
+		
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		
+		lastArticleId = id;
+		
+		return article;
+	}
+	
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+}
---
+++ src/main/java/com/kss/exam/demo/vo/Article.java
@@ -0,0 +1,14 @@
+package com.kss.exam.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //@Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode을 한꺼번에 설정
+@NoArgsConstructor // Class 모든 필드 값을 파라미터로 받는 생성자를 추가합니다.
+@AllArgsConstructor // Class 기본 생성자를 자동으로 추가해줍니다.
+public class Article {
+	int id;
+	String title;
+	String body;
+}
