4411e1d82931fab48a80ecbcaf57ea4ca3a32589
ksscok <ksscok1@gmail.com>
2022-11-21 23:58:54 +0900

    C:/Program Files/Git/usr/member/doJoin에 ResultData 적용

--- src/main/java/com/kss/exam/demo/service/MemberService.java
+++ src/main/java/com/kss/exam/demo/service/MemberService.java
@@ -3,41 +3,45 @@ package com.kss.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.kss.exam.demo.repository.MemberRepository;
+import com.kss.exam.demo.util.Ut;
 import com.kss.exam.demo.vo.Member;
+import com.kss.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
+
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if(oldMember != null) {
-			return -1;
+
+		if (oldMember != null) {
+			return ResultData.from("F-7", Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId));
 		}
-		
+
 		oldMember = getMemberByNameAndEmail(name, email);
-		
-		if(oldMember != null) {
-			return -2;
+
+		if (oldMember != null) {
+			return ResultData.from("F-8", Ut.f("해당 이름(%s)과 이메일(%s)는 이미 사용중입니다.", name, email));
 		}
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	public Member getMemberById(int id) {
 		return memberRepository.getMemberById(id);
 	}
-	
+
 	public Member getMemberByLoginId(String loginId) {
 		return memberRepository.getMemberByLoginId(loginId);
 	}
-	
+
 	public Member getMemberByNameAndEmail(String name, String email) {
 		return memberRepository.getMemberByNameAndEmail(name, email);
 	}
