a9880f99cc9ef04598f4c38e71240e3fae10bbbc
ksscok <ksscok1@gmail.com>
2022-11-22 01:33:05 +0900

    로그인, 로그아웃 기능 구현

--- src/main/java/com/kss/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kss/exam/demo/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.kss.exam.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -49,5 +51,56 @@ public class UsrMemberController {
 		Member member = memberService.getMemberById(joinRd.getData1());
 		
 		return ResultData.newData(joinRd, member);
+	}
+	
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData<Member> doLogin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if(isLogined) {
+			return ResultData.from("F-A", "이미 로그인 중입니다.");
+		}
+		
+		if(Ut.empty(loginId)) {
+			return ResultData.from("F-1", "loginId를 입력해주세요.");
+		}
+		if(Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "loginPw를 입력해주세요.");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if(member == null) {
+			return ResultData.from("F-3", "존재하지 않는 아이디입니다.");
+		}
+		if(member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+		}
+		httpSession.setAttribute("loginedMemberId", member.getId());
+		
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()), member);
+	}
+	
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public ResultData<Member> doLogout(HttpSession httpSession) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if(isLogined == false) {
+			return ResultData.from("F-A", "로그인 되어있지 않습니다.");
+		}
+		
+		httpSession.removeAttribute("loginedMemberId");
+		
+		return ResultData.from("S-1", "로그아웃 되었습니다.");
 	}
 }
