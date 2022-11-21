eaeef11b36f839dc3b8df4e8201113a3127784b4
ksscok <ksscok1@gmail.com>
2022-11-21 23:25:40 +0900

    C:/Program Files/Git/usr/article/doAdd에 ResultData 적용

--- src/main/java/com/kss/exam/demo/service/ArticleService.java
+++ src/main/java/com/kss/exam/demo/service/ArticleService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kss.exam.demo.repository.ArticleRepository;
+import com.kss.exam.demo.util.Ut;
 import com.kss.exam.demo.vo.Article;
+import com.kss.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -18,9 +20,12 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 	
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
+		
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 	
 	public Article getArticleById(int id) {
