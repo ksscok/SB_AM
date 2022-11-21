3c41617e766c26a2dd1571fa62a6ae68d93fafe7
ksscok <ksscok1@gmail.com>
2022-11-21 18:41:08 +0900

    MemberController, MemberService, MemberRepository 추가, 회원가입 로직 구현

---
+++ src/main/java/com/kss/exam/demo/controller/UsrMemberController.java
@@ -0,0 +1,27 @@
+package com.kss.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kss.exam.demo.service.MemberService;
+import com.kss.exam.demo.vo.Member;
+
+@Controller
+public class UsrMemberController {	
+	private MemberService memberService;
+	
+	public UsrMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		Member member = memberService.getMemberById(id);
+
+		return member;
+	}
+}
---
+++ src/main/java/com/kss/exam/demo/service/MemberService.java
@@ -0,0 +1,25 @@
+package com.kss.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kss.exam.demo.repository.MemberRepository;
+import com.kss.exam.demo.vo.Member;
+
+@Service
+public class MemberService {
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
+	}
+
+}
