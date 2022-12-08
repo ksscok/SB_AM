95ed57481633e298a379061a044e214154b29c5c
ksscok <ksscok1@gmail.com>
2022-12-08 21:59:45 +0900

    경고창 메시지가 ?로 나올 때 대처, 게시물리스트에서 존재하지 않는 게시판 체크

--- src/main/java/com/kss/exam/demo/vo/Rq.java
+++ src/main/java/com/kss/exam/demo/vo/Rq.java
@@ -75,10 +75,12 @@ public class Rq {
 	}
 	
 	public String jsHistoryBack(String msg) {
+		resp.setContentType("text/html; charset=UTF-8");
 		return Ut.jsHistoryBack(msg);
 	}
 
 	public String jsReplace(String msg, String uri) {
+		resp.setContentType("text/html; charset=UTF-8");
 		return Ut.jsReplace(msg, uri);
 	}
 
