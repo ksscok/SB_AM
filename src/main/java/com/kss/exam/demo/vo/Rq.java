77f4a362be0003b00732c00af4ac25a47e9df8e7
ksscok <ksscok1@gmail.com>
2022-12-08 23:04:01 +0900

    게시물 총 개수 출력

--- src/main/java/com/kss/exam/demo/vo/Rq.java
+++ src/main/java/com/kss/exam/demo/vo/Rq.java
@@ -44,6 +44,7 @@ public class Rq {
 	}
 	
 	public void printHistoryBackJs(String msg) {
+		resp.setContentType("text/html; charset=UTF-8");
 		print(Ut.jsHistoryBack(msg));
 	}
 
@@ -75,12 +76,10 @@ public class Rq {
 	}
 	
 	public String jsHistoryBack(String msg) {
-		resp.setContentType("text/html; charset=UTF-8");
 		return Ut.jsHistoryBack(msg);
 	}
 
 	public String jsReplace(String msg, String uri) {
-		resp.setContentType("text/html; charset=UTF-8");
 		return Ut.jsReplace(msg, uri);
 	}
 
