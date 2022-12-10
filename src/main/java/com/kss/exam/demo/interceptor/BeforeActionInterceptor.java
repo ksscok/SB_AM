59bd5a745d09a26a064cf9eaac8f525253b3bff7
ksscok <ksscok1@gmail.com>
2022-12-10 17:14:55 +0900

    BeforeActionInterceptor에서 Rq 객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/kss/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/kss/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -12,11 +12,18 @@ import com.kss.exam.demo.vo.Rq;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+	private Rq rq;
+	
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
+	
 	@Autowired
 	private MemberService memberService;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		rq.initOnBeforeActionInterceptor();
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
