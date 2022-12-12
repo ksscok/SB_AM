1f2d351aebce1e0226843ac043f61837e99d42a0
ksscok <ksscok1@gmail.com>
2022-12-12 19:20:22 +0900

    리스트에서 검색타입과 검색어에 따라, 총 게시물 수 계산

--- src/main/java/com/kss/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kss/exam/demo/controller/UsrArticleController.java
@@ -53,7 +53,11 @@ public class UsrArticleController {
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
+	public String showList(Model model, 
+			@RequestParam(defaultValue = "1") int boardId, 
+			@RequestParam(defaultValue = "title, body") String searchKeywordTypeCode,
+			@RequestParam(defaultValue = "") String searchKeyword, 
+			@RequestParam(defaultValue = "1") int page) {
 
 		Board board = boardService.getBoardById(boardId);
 		
@@ -61,7 +65,7 @@ public class UsrArticleController {
 			return rq.historyBackJsOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
 		}
 		
-		int articlesCount = articleService.getArticlesCount(boardId);
+		int articlesCount = articleService.getArticlesCount(boardId, searchKeywordTypeCode, searchKeyword);
 		
 		int itemsCountInAPage = 10;
 		int pagesCount = (int) Math.ceil((double)articlesCount/itemsCountInAPage);
