168a572b043e89fdd80253150a1de740b4055cc3
ksscok <ksscok1@gmail.com>
2022-12-19 23:10:58 +0900

    댓글 수정 폼, 처리

--- src/main/java/com/kss/exam/demo/controller/UsrReplyController.java
+++ src/main/java/com/kss/exam/demo/controller/UsrReplyController.java
@@ -1,11 +1,14 @@
 package com.kss.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kss.exam.demo.service.ArticleService;
 import com.kss.exam.demo.service.ReplyService;
 import com.kss.exam.demo.util.Ut;
+import com.kss.exam.demo.vo.Article;
 import com.kss.exam.demo.vo.Reply;
 import com.kss.exam.demo.vo.ResultData;
 import com.kss.exam.demo.vo.Rq;
@@ -13,10 +16,12 @@ import com.kss.exam.demo.vo.Rq;
 @Controller
 public class UsrReplyController {	
 	private ReplyService replyService;
+	private ArticleService articleService;
 	private Rq rq;
 
-	public UsrReplyController(ReplyService replyService, Rq rq) {
+	public UsrReplyController(ReplyService replyService, ArticleService articleService, Rq rq) {
 		this.replyService = replyService;
+		this.articleService = articleService;
 		this.rq = rq;
 	}
 
@@ -26,18 +31,18 @@ public class UsrReplyController {
 		if ( Ut.empty(relTypeCode) ) {
 			return rq.jsHistoryBack("relTypeCode(을)를 입력해주세요.");
 		}
-
+		
 		if ( Ut.empty(relId) ) {
 			return  rq.jsHistoryBack("relId(을)를 입력해주세요.");
 		}
-
+		
 		if ( Ut.empty(body) ) {
 			return  rq.jsHistoryBack("body(을)를 입력해주세요.");
 		}	
-
+		
 		ResultData<Integer> writeReplyRd = replyService.writeReply(rq.getLoginedMemberId(), relTypeCode, relId, body);
 		int id = writeReplyRd.getData1();
-
+		
 		if ( Ut.empty( replaceUri )) {
 			switch (relTypeCode) {
 			case "article":
@@ -45,31 +50,28 @@ public class UsrReplyController {
 				break;
 			}
 		}
-
+		
+		
 		return rq.jsReplace(writeReplyRd.getMsg(), replaceUri);
 	}
 	
 	@RequestMapping("/usr/reply/doDelete")
 	@ResponseBody
 	public String doDelete(int id, String replaceUri) {
-		if(Ut.empty(id)) {
-			return rq.jsHistoryBack("id(을)를 입력해주세요");
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
 		}
-
+		
 		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
-
-		if (reply == null) {
-			return rq.jsHistoryBack(Ut.f("%d번 댓글이 존재하지 않습니다.", id));
+		
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
 		}
-
-		if (rq.isLogined() == false) {
-			return rq.jsHistoryBack("로그인 후 이용해주세요.");
-		}
-
+		
 		if (reply.getMemberId() != rq.getLoginedMemberId()) {
-			return rq.jsHistoryBack("권한이 없습니다.");
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 삭제할 권한이 없습니다.", id));
 		}
-
+		
 		ResultData deleteReplyRd = replyService.deleteReply(id);
 		
 		if ( Ut.empty( replaceUri )) {
@@ -81,5 +83,70 @@ public class UsrReplyController {
 		}
 
 		return rq.jsReplace(deleteReplyRd.getMsg(), replaceUri);
+	}
+
+	@RequestMapping("/usr/reply/modify")
+	public String modify(int id, String replaceUri, Model model) {
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
+		}
+
+		if (reply.getMemberId() != rq.getLoginedMemberId()) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
+		}
+
+		String relDataTitle = null;
+
+		switch (reply.getRelTypeCode()) {
+		case "article":
+			Article article = articleService.getArticle(reply.getRelId());
+			relDataTitle = article.getTitle();
+		}
+
+		model.addAttribute("relDataTitle", relDataTitle);
+		model.addAttribute("reply", reply);
+
+
+		return "usr/reply/modify";
+	}
+	
+	@RequestMapping("/usr/reply/doModify")
+	@ResponseBody
+	public String doModify(int id, String body, String replaceUri) {
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
+		}
+
+		if (reply.getMemberId() != rq.getLoginedMemberId()) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
+		}
+
+		if ( Ut.empty(body) ) {
+			return rq.jsHistoryBack("body(을)를 입력해주세요.");
+		}
+
+		ResultData modifyReplyRd = replyService.modifyReply(id, body);
+
+		if ( Ut.empty( replaceUri )) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+
+		return rq.jsReplace(modifyReplyRd.getMsg(), replaceUri);
 	}
 }
--- src/main/webapp/WEB-INF/jsp/usr/article/modify.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/modify.jsp
@@ -4,31 +4,55 @@
 <c:set var="pageTitle" value="게시물 내용" />
 <%@ include file="../common/head.jspf" %>
 
+<script>
+  let ArticleModify__submitDone = false;
+  function ArticleModify__submitDone(form) {
+    if (ArticleModify__submitDone) {
+      return;
+    }
+    // 좌우공백 제거
+    form.body.value = form.body.value.trim();
+    if (form.body.value.length == 0) {
+      alert('내용을 입력해주세요.');
+      form.body.focus();
+      return;
+    }
+    ArticleModify__submitDone = true;
+    form.submit();
+  }
+</script>
+
 <section class="mt-5">
-	<div class="container mx-auto px-3">
-		<form class="table-box-type-1" method="POST" action="../article/doModify">
-			<input type="hidden" name="id" value="${article.id}" />
-			<table>
-				<colgroup>
-					<col width="200"/>	
-				</colgroup>
-				<tbody>
-					<tr>
-						<th>번호</th>
-						<td><div class="badge badge-primary">${article.id}</div></td>
-					</tr>
-					<tr>
-						<th>작성날짜</th>
+  <div class="container mx-auto px-3">
+    <form class="table-box-type-1" method="POST" action="../article/doModify" onsubmit="ArticleModify__submitDone">
+      <input type="hidden" name="id" value="${article.id}" />
+      <table>
+        <colgroup>
+          <col width="200" />
+        </colgroup>
+        <tbody>
+          <tr>
+            <th>번호</th>
+            <td>
+              <div class="badge badge-primary">${article.id}</div>              
+            </td>
+          </tr>
+          <tr>
+            <th>작성날짜</th>
             <td>
               ${article.forPrintType2RegDate}
             </td>
-					</tr>
-					<tr>
-						<th>수정날짜</th>
-						<td>
+          </tr>
+          <tr>
+            <th>수정날짜</th>
+            <td>
               ${article.forPrintType2UpdateDate}
             </td>
-					</tr>
+          </tr>
+          <tr>
+            <th>작성자</th>
+            <td>${article.extra__writerName}</td>
+          </tr>
           <tr>
             <th>조회</th>
             <td>
@@ -41,33 +65,35 @@
               <span class="text-blue-700">${article.goodReactionPoint}</span>
             </td>
           </tr>
-					<tr>
-						<th>작성자</th>
-						<td>${article.memberId}</td>
-					</tr>
-					<tr>
-	          <th>제목</th>
-	          <td>
-	            <input name="title" type="text" placeholder="제목" class="w-96 input input-bordered w-full max-w-xs" value="${article.title}"/>
-	          </td>
-	        </tr>
-	        <tr>
-	          <th>내용</th>
-	          <td>
-	            <textarea class="w-full textarea textarea-bordered" name="body" rows="10">${article.body}</textarea>
-	          </td>
-	        </tr>
-	        <tr>
-	          <th>수정</th>
-	          <td>
-	            <input type="submit" class="btn btn-primary" value="수정" />
-	            <button type="button" class="btn btn-outline btn-success" onclick="history.back();">뒤로가기</button>
-	          </td>
-	        </tr>
-				</tbody>
-			</table>
-		</form>
-	</div>
+          <tr>
+            <th>제목</th>
+            <td>
+              <input name="title" type="text" placeholder="제목" class="w-96 input input-bordered w-full max-w-xs" value="${article.title}"/>
+            </td>
+          </tr>
+          <tr>
+            <th>내용</th>
+            <td>
+              <textarea class="w-full textarea textarea-bordered" name="body" rows="10" placeholder="내용">${article.body}</textarea>
+            </td>
+          </tr>
+          <tr>
+            <th>수정</th>
+            <td>
+              <input type="submit" class="btn btn-primary" value="수정" />
+              <button type="button" class="btn btn-outline btn-success" onclick="history.back();">뒤로가기</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="btns">
+        <a class="btn btn-link" href="../article/modify?id=${article.id}">게시물 수정</a>
+        <c:if test="${article.extra__actorCanDelete}">
+          <a class="btn btn-link" onclick="if ( confirm('정말 삭제하시겠습니까?') == false) return false;"
+            href="../article/doDelete?id=${article.id}">게시물 삭제</a>
+        </c:if>
+      </div>
+     </form>
+  </div>
 </section>
-	
-<%@ include file="../common/foot.jspf" %>
+<%@ include file="../common/foot.jspf"%>
